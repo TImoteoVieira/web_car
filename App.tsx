bd46a40f17334b73c36f5cf3cda4dd7cebbfc9aa
TImoteoVieira <timoteodeveloper@gmail.com>
2025-01-13 17:55:07 -0300

    refactor(App): reorganiza estrutura principal do aplicativo

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { StatusBar } from 'react-native'
+import { Routes } from './src/routes'
+import { ToastProvider } from './src/contexts/ToastContext'
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <ToastProvider>
+        <StatusBar backgroundColor="#F3f5f8" barStyle="dark-content" />
+        <Routes />
+      </ToastProvider>
+    </NavigationContainer>
+  );
+}
+
